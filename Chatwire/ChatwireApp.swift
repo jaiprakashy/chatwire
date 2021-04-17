e897fa606eb88c104f7db2ecd41ac880a90510f1
Jaiprakash Yadav <yadavjaiprakash20@gmail.com>
2021-04-17 17:54:06 +0530

    Initial Commit

---
+++ Chatwire/ChatwireApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatwireApp.swift
+//  Chatwire
+//
+//  Created by Jaiprakash Yadav on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatwireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatwire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chatwire
+//
+//  Created by Jaiprakash Yadav on 17/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
